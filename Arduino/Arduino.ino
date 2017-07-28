4a6adcb687c01c7feb8037553586a13835b4d28a
Flo <flohacksfriday@gmail.com>
2017-07-28 18:14:07 +0200

    Improve Arduino Firmware.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,10 +1,13 @@
 /*
-* This file is just a Prototype Firmware for pixelflow. It will only work on the
-* Arduino Mega since the Arduino Uno has to little RAM (even if it compiles on
-* the Uno).
-* Unfortently the Performace is not the best so it's recommended to wait one
-* Second between two frames.
+* Author: flofriday
+* Date: 28.07.2017
+* License: MIT
+*
+* This file is the code for the Arduino.
+* Supported are Arduino Uno and Arduino Mega however, alomost every Arduino
+* should work.
 */
+
 
 /*
 * Configure the Prameters below to fit your application.
@@ -12,52 +15,70 @@
 #define MATRIX_DATA_PIN 6       // The Pin where the matrix is connected to.
 #define MATRIX_SNAKE_PATTERN 1  // Set 1 if the Matrix has a snake-pattern.
 #define MAX_BRIGHTNESS 50       // The maximum brightness the firmware allow.
+#define BAUDRATE 250000         // Set the Baudrate (higher is better)
 
 
-
+/*
+* Include all headerfiles.
+*/
 #include <stdint.h>
 #include <string.h>
 #include <Adafruit_NeoPixel.h>
+
+
+/*
+* Define the LED Matrix.
+*/
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(256, MATRIX_DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 
+/*
+* Setup the Matrix and the serial communication.
+*/
 void setup()
 {
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  Serial.begin(250000);
   strip.setBrightness(MAX_BRIGHTNESS);
   strip.show();
+  Serial.begin(BAUDRATE);
 }
 
 
-#define cmdBrightness '0'
-#define cmdFrame '1'
-#define cmdRow '2'
-#define cmdColum '3'
-#define cmdPixel '4'
-#define cmdSave '5'
+/*
+* Some constants wich defines the communication.
+*/
+#define cmdBrightness '1'
+#define cmdPixel '2'
+#define cmdFrame '3'
+#define cmdSave '4'
 #define cmdBrightnessLength 4
 #define cmdFrameLength 1537
-#define cmdRowLength
-#define cmdColumLength
 #define cmdPixelLength 9
-#define cmdSaveLength
+#define cmdSaveLength // not implemented yet
 
 
-char buffer[2000] = "";
+/*
+* All variables needed to work with the buffer.
+*/
+#define BUFFER_SIZE 1600
+char buffer[BUFFER_SIZE] = "";
 uint16_t bufferLength = 0;
 
+
+/*
+* This function sets the brightness of the whole Matrix.
+*/
 void setBrightness()
 {
   // Checking if the command has the right length
   if (bufferLength != cmdBrightnessLength)
   {
     // The command was too long or too short
-    Serial.println("ERROR: updateBrightness wrong length.");
+    Serial.println("ERROR: Command \"setBrightness\" had the wrong length!");
     return;
   }
 
+  // convert the buffer to an int.
   uint16_t brightness = (buffer[1] - '0') * 100;
   brightness += (buffer[2] - '0') * 10;
   brightness += buffer[3] - '0';
@@ -66,64 +87,12 @@ void setBrightness()
   if (brightness < 1 || brightness > MAX_BRIGHTNESS)
   {
     // The command was too long or too short
-    Serial.println("ERROR: updateBrightness too big value");
+    Serial.println("ERROR: Command \"setBrightness\" had the wrong length!");
     return;
   }
 
   // The command was correct so set the brightness of the strip
   strip.setBrightness(brightness);
-  strip.show();
-}
-
-
-void drawFrame()
-{
-  // Checking if the command has the right length
-  if (bufferLength != cmdFrameLength)
-  {
-    // The command was too long or too short
-    Serial.println("ERROR: drawFrame wrong length.");
-    return;
-  }
-
-  uint8_t curPixel = 0;
-  uint8_t curColum = 0;
-  uint8_t curRow = 0;
-  uint8_t red = 0;
-  uint8_t green = 0;
-  uint8_t blue = 0;
-  uint16_t offset = 1;
-  char strRed[3] = "";
-  char strGreen[3] = "";
-  char strBlue[3] = "";
-
-  for (uint8_t r = 0; r < 16; r++)
-  {
-    for (uint8_t c = 0; c < 16; c++)
-    {
-      //#if MATRIX_SNAKE_PATTERN == 1
-      curColum = c;
-      if (r % 2 == 1) curColum = 15 - curColum;
-      //#endif
-
-      curPixel = (r << 4) + curColum; // shift by 4 is like multiply by 16 but faster
-
-      strRed[0] = buffer[offset + 0];
-      strRed[1] = buffer[offset + 1];
-      strGreen[0] = buffer[offset + 2];
-      strGreen[1] = buffer[offset + 3];
-      strBlue[0] = buffer[offset + 4];
-      strBlue[1] = buffer[offset + 5];
-      offset += 6;
-
-      red = (int)strtol(strRed, NULL, 16);
-      green = (int)strtol(strGreen, NULL, 16);
-      blue = (int)strtol(strBlue, NULL, 16);
-
-      strip.setPixelColor(curPixel, red, green, blue);
-    }
-  }
-
   strip.show();
 }
 
@@ -139,7 +108,7 @@ void drawPixel()
   if (bufferLength != cmdPixelLength)
   {
     // The command was too long or too short
-    Serial.println("ERROR: drawPixel wrong length.");
+    Serial.println("ERROR: Command \"drawPixel\" had the wrong length!");
     return;
   }
 
@@ -156,22 +125,26 @@ void drawPixel()
   char strRow[2] = "";
   char strColum[2] = "";
 
+  // Create the strings for the row & colum
   strRow[0] =  buffer[1];
   strColum[0] =  buffer[2];
 
+  // Convert the strings to ints
   row = (uint8_t)strtol(strRow, NULL, 16);
   colum = (uint8_t)strtol(strColum, NULL, 16);
 
+  // Fix the snake pattern of the leds on the matrix if needed
   #if MATRIX_SNAKE_PATTERN == 1
-  // fix the snake pattern of the leds on the matrix
   if (row % 2 == 1)
   {
     colum = 15 - colum;
   }
   #endif
 
+  // Calculate the current pixel
   stripPixel = (row << 4) + colum;  // shift by 4 is like multiply by 16 but faster
 
+  // Create the strings for the color
   strRed[0] = buffer[3];
   strRed[1] = buffer[4];
   strGreen[0] = buffer[5];
@@ -179,11 +152,77 @@ void drawPixel()
   strBlue[0] = buffer[7];
   strBlue[1] = buffer[8];
 
+  // Convert the strings to ints
   red = (uint8_t)strtol(strRed, NULL, 16);
   green = (uint8_t)strtol(strGreen, NULL, 16);
   blue = (uint8_t)strtol(strBlue, NULL, 16);
 
+  // Set the pixel color and update the matrix.
   strip.setPixelColor(stripPixel, red, green, blue);
+  strip.show();
+}
+
+
+/*
+* This function reads the hex color values from the buffer and writes the frame
+* to the matrix.
+*/
+void drawFrame()
+{
+  // Checking if the command has the right length
+  if (bufferLength != cmdFrameLength)
+  {
+    // The command was too long or too short
+    Serial.println("ERROR: Command \"drawFrame\" had the wrong length!");
+    return;
+  }
+
+  // Declare all variables needed for this function
+  uint8_t curPixel = 0;
+  uint8_t curColum = 0;
+  uint8_t red = 0;
+  uint8_t green = 0;
+  uint8_t blue = 0;
+  uint16_t offset = 1;
+  char strRed[3] = "";
+  char strGreen[3] = "";
+  char strBlue[3] = "";
+
+  // Loop through all 256 pixels (each 6 Byte)
+  for (uint8_t r = 0; r < 16; r++)
+  {
+    for (uint8_t c = 0; c < 16; c++)
+    {
+      curColum = c;
+
+      // Fix the snake pattern if needed
+      #if MATRIX_SNAKE_PATTERN == 1
+      if (r % 2 == 1) curColum = 15 - curColum;
+      #endif
+
+      // calculate the current pixel
+      curPixel = (r << 4) + curColum; // shift by 4 is like multiply by 16 but faster
+
+      // Set the strings for the colors
+      strRed[0] = buffer[offset + 0];
+      strRed[1] = buffer[offset + 1];
+      strGreen[0] = buffer[offset + 2];
+      strGreen[1] = buffer[offset + 3];
+      strBlue[0] = buffer[offset + 4];
+      strBlue[1] = buffer[offset + 5];
+      offset += 6;
+
+      // convert the strings to numbers
+      red = (int)strtol(strRed, NULL, 16);
+      green = (int)strtol(strGreen, NULL, 16);
+      blue = (int)strtol(strBlue, NULL, 16);
+
+      // Set the current pixel
+      strip.setPixelColor(curPixel, red, green, blue);
+    }
+  }
+
+  // Update the matrix
   strip.show();
 }
 
@@ -197,10 +236,8 @@ void analyseBuffer()
   {
     case cmdBrightness: setBrightness(); break;
     case cmdFrame: drawFrame(); break;
-    //case cmdRow: break;
-    //case cmdColum: break;
     case cmdPixel: drawPixel(); break;
-    //case cmdSave: break;
+    //case cmdSave: break; // Not implemented yet
     default: Serial.println("Error: Unknown Command!"); break;
   }
 }
