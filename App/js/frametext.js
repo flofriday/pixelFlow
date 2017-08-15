55f0cd491a53cda77dcd8443d1b5f62402e77029
flofriday <flohacksfriday@gmail.com>
2017-08-15 21:47:41 +0200

    Add file for creating text

---
+++ App/js/frametext.js
@@ -0,0 +1,15 @@
+
+const biu = require('biu.js')
+
+var btnText = document.getElementById('btn-text-frame')
+
+btnText.addEventListener('click', enterText)
+
+function enterText() {
+  //TODO: Replace the message with a working code
+  biu('This function is not supported.', {type: 'warning', pop: true, el: document.getElementById('window')})
+}
+
+function drawText() {
+
+}
