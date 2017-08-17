c7fe9b7ab0b14b62f494445e5bea1aeeb4cdd5ef
flofriday <flohacksfriday@gmail.com>
2017-08-17 13:42:11 +0200

    Outsource the creation of child windows

--- App/js/index.js
+++ App/js/index.js
@@ -2,10 +2,8 @@
 const electron = require('electron')
 const remote = electron.remote
 const webFrame = electron.webFrame
-const BrowserWindow = electron.remote.BrowserWindow
 const ipc = electron.ipcRenderer
 const os = require('os')
-const path = require('path')
 const windowConfig = require('../js/windowconfig')
 const matrix = require('../js/mainmatrix')
 matrix.start()  //start it up
@@ -16,6 +14,7 @@ const player = require('../js/player')
 const hardware = require('../js/hardware')
 const frameText = require('../js/frametext')
 const app_menu = require('../js/app_menu');
+const windowMaker = require('../js/windowmaker')
 
 
 // Set up colorpicker
@@ -30,8 +29,8 @@ var allLinks = document.querySelectorAll('a[href]')
 
 
 btnDeveloper.addEventListener("click", function() {remote.getCurrentWindow().toggleDevTools()})
-btnAbout.addEventListener("click", openAbout)
-btnBluetooth.addEventListener("click", openConnection)
+btnAbout.addEventListener("click", windowMaker.openAbout)
+btnBluetooth.addEventListener("click", windowMaker.openConnection)
 document.addEventListener("keydown", globalKeyHandler)
 
 
@@ -49,87 +48,10 @@ function globalKeyHandler(e) {
     if (webFrame.getZoomFactor() < 0.25) {webFrame.setZoomFactor(0.25)}
     console.log("Zoom: " + (webFrame.getZoomFactor() * 100) + "%")
   }
-  else if((e.key === "r" && (e.ctrlKey || e.metaKey) ) || e.keyCode === 116) {
-    // Reload ouput
+  else if(e.keyCode === 116) {
+    // Reload ouput (F5)
     hardware.updateFrame(matrix.getPixelList())
   }
-}
-
-
-// Create a new Window when the about button clicked
-var aboutWinId = null
-function openAbout() {
-  // check if the aboutwindow is just in background
-  if (aboutWinId !== null)
-  {
-    //just focus the opend window
-    BrowserWindow.fromId(aboutWinId).focus()
-    return
-  }
-
-  const modalPath = path.join('file://', __dirname, 'about.html')
-  let win = new BrowserWindow({
-    width: 600,
-    height: 450,
-    frame: false,
-    show: false,
-    resizable: false,
-    minimizable: false,
-    titleBarStyle: 'hidden',
-  })
-  win.loadURL(modalPath)
-  //win.webContents.openDevTools()  // Only for debugging
-  win.once('ready-to-show', function () { win.show() })
-  win.on('close', function () { win = null; aboutWinId = null })
-  aboutWinId = win.id
-}
-
-
-// Function to get the Path for a Icon
-function getIconPath() {
-  if (os.platform() === 'darwin') {
-    return path.join(__dirname, '../img/icon.icns')
-  }
-  else if (os.platform() === 'win32') {
-    return path.join(__dirname, '../img/icon.ico')
-  }
-  else {
-    // For all other platforms like: linux, freebsd, openbsd
-    return path.join(__dirname, '../img/icon.png')
-  }
-}
-
-
-// Create a new Window when the Connection button clicked
-var connectWinId = null
-function openConnection() {
-  // check if the connectionwindow is allready open
-  if (connectWinId != null)
-  {
-    //close the window
-    BrowserWindow.fromId(connectWinId).close()
-    connectWinId = null
-    return
-  }
-
-  const modalPath = path.join('file://', __dirname, 'connection.html')
-  let win = new BrowserWindow({
-    width: 600,
-    height: 400,
-    resizable: false,
-    minimizable: false,
-    title: "Select Hardware",
-    icon: getIconPath(),
-    backgroundColor: '#ffffff',
-    parent: BrowserWindow.getFocusedWindow(),
-    show: false
-  })
-  win.setMenu(null)
-  win.loadURL(modalPath)
-  //win.webContents.openDevTools()  // Only for debugging
-  win.once('ready-to-show', function () { win.show() })
-  win.on('close', function () { win = null; connectWinId = null })
-  connectWinId = win.id
 }
 
 // The drag and drop support
---
+++ App/js/windowmaker.js
@@ -0,0 +1,97 @@
+/*
+* This file is reponsable to open windows like the about or connection window.
+*/
+
+/*
+* Load needed modules
+*/
+const electron = require('electron')
+const remote = electron.remote
+const BrowserWindow = electron.remote.BrowserWindow
+const path = require('path')
+
+/*
+* Create the windows
+*/
+
+// Create the About Window
+var aboutWinId = null
+function openAbout() {
+  // check if the aboutwindow is just in background
+  if (aboutWinId !== null)
+  {
+    //just focus the opend window
+    BrowserWindow.fromId(aboutWinId).focus()
+    return
+  }
+
+  const modalPath = path.join('file://', __dirname, '../html/about.html')
+  let win = new BrowserWindow({
+    width: 600,
+    height: 450,
+    frame: false,
+    show: false,
+    resizable: false,
+    minimizable: false,
+    titleBarStyle: 'hidden',
+  })
+  win.loadURL(modalPath)
+  //win.webContents.openDevTools()  // Only for debugging
+  win.once('ready-to-show', function () { win.show() })
+  win.on('close', function () { win = null; aboutWinId = null })
+  aboutWinId = win.id
+}
+
+
+// Function to get the Path for a Icon
+function getIconPath() {
+  if (os.platform() === 'darwin') {
+    return path.join(__dirname, '../img/icon.icns')
+  }
+  else if (os.platform() === 'win32') {
+    return path.join(__dirname, '../img/icon.ico')
+  }
+  else {
+    // For all other platforms like: linux, freebsd, openbsd
+    return path.join(__dirname, '../img/icon.png')
+  }
+}
+
+
+// Create the Connection Window
+var connectWinId = null
+function openConnection() {
+  // check if the connectionwindow is allready open
+  if (connectWinId != null)
+  {
+    //close the window
+    BrowserWindow.fromId(connectWinId).close()
+    connectWinId = null
+    return
+  }
+
+  const modalPath = path.join('file://', __dirname, '../html/connection.html')
+  let win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    resizable: false,
+    minimizable: false,
+    title: "Select Hardware",
+    icon: getIconPath(),
+    backgroundColor: '#ffffff',
+    parent: BrowserWindow.getFocusedWindow(),
+    show: false
+  })
+  win.setMenu(null)
+  win.loadURL(modalPath)
+  //win.webContents.openDevTools()  // Only for debugging
+  win.once('ready-to-show', function () { win.show() })
+  win.on('close', function () { win = null; connectWinId = null })
+  connectWinId = win.id
+}
+
+/*
+* Export the Module
+*/
+module.exports.openAbout = openAbout
+module.exports.openConnection = openConnection
